4b10103d67a71cd7ce8dd693ecb605474133b32a
LucEast <47122750+LucEast@users.noreply.github.com>
2022-10-22 15:31:55 +0200

    still protoype

--- contentdl.py
+++ contentdl.py
@@ -10,9 +10,12 @@ except:
     print("Install requirements with 'pip3 install -r requirements'")
 
 questions = [inquirer.Path(
-    'coursefile', message='Please enter path of the ".md" course-file', exists=True, path_type=Path.FILE)]
+    'coursefile', message='Please enter path of the course-file', exists=True, path_type=Path.FILE),
+    inquirer.Path('dest_path', message="Where do you want to save the content?", exists=True, path_type=Path.DIRECTORY)]
 
-answers = inquirer.prompt(questions)['coursefile']
+answers = inquirer.prompt(questions)
+input_file = answers['coursefile']
+output_dir = answers['dest_path']
 
 
 class course:
@@ -20,27 +23,28 @@ class course:
         self.file = file
 
     def find_md_links(self):
+        # inline_link_re = re.compile(
+        #     r'\[!(\[([^\]]+)\])\(([^)]+)\)]\(([^)]+)\)')
         inline_link_re = re.compile(
-            r'\[!(\[([^\]]+)\])\(([^)]+)\)]\(([^)]+)\)')
+            r'((https?):((//secasio.de)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)')
         # links = list(inline_link_re.findall(self.file))
         # print(links)
         with open(self.file, 'r') as f:
             links = inline_link_re.findall(f.read())
-            indices_to_access = [1, 3]
             accessed_lists = []
             for link in links:
-                link_map = map(link.__getitem__, indices_to_access)
-                accessed_lists.append(list(link_map))
-                # print(accessed_list)
-            return accessed_lists
+                accessed_lists.append(link[0])
+                # print(link[0])
+            return(accessed_lists)
 
     def get_content(self):
         accessed_lists = self.find_md_links()
         for accessed_list in accessed_lists:
+            complete_name = os.path.join(output_dir, accessed_list[0])
             response = requests.get(accessed_list[1])
-            open(accessed_list[0], "wb").write(response.content)
+            open(complete_name, "wb").write(response.content)
 
 
 if __name__ == "__main__":
-    cfile = course(answers)
+    cfile = course(input_file)
     cfile.get_content()
